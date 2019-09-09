6925d34b87c490ae63f01cb09952cb6996033b3e
Francisco <54814853+Frank-Sosa@users.noreply.github.com>
2019-09-09 12:24:05 -0600

    Add files via upload

---
+++ FranciscoSosaEje13.cpp
@@ -0,0 +1,37 @@
+/*Ejercicio 13 */
+#include <iostream>
+
+using namespace std;
+
+int func(int);
+
+int main(){
+
+int n;
+cout<<"Digite un numero: ";
+cin>>n;
+//cout<<func(n);
+
+int i=n-1;
+if(i==0){
+    return 0;
+}
+while(i>=0)
+{
+    n = n + i;
+    i--;
+}
+    
+cout<< n << endl;
+
+return 0;
+}
+
+/*int func(int n)
+{
+ 
+  if(n==0)
+  return(0);
+  return(n + func(n-1));
+ 
+}*/
